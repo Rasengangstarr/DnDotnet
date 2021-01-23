8f561f80fb528c482fac02afead2d33fdc86530c
Rasengangstarr <davidouthere@msn.com>
2021-01-23 01:17:01 +0000

    basic attacks work

--- Program.fs
+++ Program.fs
@@ -2,9 +2,41 @@
 
 open System
 
+// let challengeRatings =
+//     [4,2; 8,3; 12,4; 16,5; 20,6; 24,7; 28,8; 30,9;]
+
+let ChalToProf(x : float) =
+    if x <= 4.0 then 2 else
+    if x <= 8.0 then 3 else
+    if x <= 12.0 then 4 else
+    if x <= 16.0 then 5 else
+    if x <= 20.0 then 6 else
+    if x <= 24.0 then 7 else
+    if x <= 28.0 then 8 else
+    if x <= 30.0 then 9 else
+    0
+
+let objrandom = new Random()
+
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
+
+let roll dice =
+    objrandom.Next(0,dice)
+
+let max (x,y) =
+    if x > y then x else y
+
+type Armor(ac) =
+    member this.AC = ac
+
+type Weapon(damageDice, damageMod, isFinesse) =
+    member this.DamageDice : int = damageDice
+    member this.DamageMod : int = damageMod
+    member this.IsFinesse : bool = isFinesse
+
+    member this.Damage = (roll this.DamageDice) + this.DamageMod
 
 
 type StatVect =
@@ -21,10 +53,13 @@ type StatVect =
 
 end
 
-type Character(stats : StatVect, name, description) =
+type Character(stats : StatVect, name, description, armor: Armor, weapon: Weapon, cr) =
     member this.Stats = stats
     member this.Name = name
     member this.Description = description
+    member this.Armor = armor
+    member this.CR : float = cr
+    member this.Weapon : Weapon = weapon
 
     member private this.StatToMod(x) =
         let xf = float ((x - 10) / 2)
@@ -38,11 +73,23 @@ type Character(stats : StatVect, name, description) =
     member this.WisMod = this.StatToMod this.Stats.Wis
     member this.ChaMod = this.StatToMod this.Stats.Cha
 
+    member this.Attack(target : Character) =
+        let weapon = this.Weapon
+        let isFinesse = weapon.IsFinesse
+        let toHit =
+            if isFinesse 
+            then max(this.Stats.Str, this.Stats.Dex) + ChalToProf this.CR 
+            else this.Stats.Str + ChalToProf this.CR
+        if toHit + roll 20 > target.Armor.AC
+        then this.Weapon.Damage
+        else 0
 
 [<EntryPoint>]
 let main argv =
-    let goblin = new Character(new StatVect(8,14,10,10,8,8), "goblin", "Small humanoid (goblinoid), Neutral Evil")
-    printfn "%i" goblin.StrMod
+    let goblin = new Character(new StatVect(8,14,10,10,8,8), "goblin", "Small humanoid (goblinoid), Neutral Evil", new Armor(15), new Weapon(6,2,true), 0.25)
+    let hobgoblin = new Character(new StatVect(13,12,12,10,10,9), "hobgoblin", "Medium humanoid (goblinoid), Lawful Evil", new Armor(18), new Weapon(10,1,false), 0.5)
+    
+    printfn "%i" (goblin.Attack hobgoblin)
     let message = from "F#" // Call the function
     printfn "Hello world %s" message
     0 // return an integer exit code
