d98379fbd2131904b5c236e875f4f78f37c751af
Rasengangstarr <davidouthere@msn.com>
2021-01-24 22:32:09 +0000

    total refactor again

--- Program.fs
+++ Program.fs
@@ -1,53 +1,10 @@
 open System
 
-let mutable combatLog : string list = []
-
-let log x =
-    combatLog <- List.append combatLog [x]
-    0
-
-let objrandom = new Random()
-
-let max (x,y) = if x > y then x else y
-
-let StatToMod x = float ((x - 10) / 2) |> floor |> int
-
-let ChalToProf(x : float) =
-    if x <= 4.0 then 2 else
-    if x <= 8.0 then 3 else
-    if x <= 12.0 then 4 else
-    if x <= 16.0 then 5 else
-    if x <= 20.0 then 6 else
-    if x <= 24.0 then 7 else
-    if x <= 28.0 then 8 else
-    if x <= 30.0 then 9 else
-    0
-
-//#region types
-
-type Details = 
-    { Name : string
-      Description : string }
-
-
-let Finesse x y = max(x,y)
-
-let roll dice =
-    let result = objrandom.Next(1,dice)
-    log (sprintf "rolled %i on a d%i" result dice) |> ignore
-    result
-    
-
-
-type Dice =
+type Details =
     {
-        Sides : int
-    }
-
-type WeaponProperties =
-    {
-        Finesse : bool
-        Ranged  : bool
+        Identifier : string
+        Name : string
+        Description : string
     }
 
 type ArmorClass =
@@ -55,181 +12,107 @@ type ArmorClass =
     | Medium = 1
     | Heavy = 2
 
-type Item = 
-    | Weapon of details:Details * dice: int * properties: WeaponProperties
-    | Armor of details:Details * modifier: int * armorClass: ArmorClass
+type ItemPropertyExecutionPoint =
+    | ToHitModCalc = 0
+    | ToHitRollCalc = 1
+    | DamageCalc = 2
 
-type Creature = 
+type ItemPropertyCompositionStyle =
+    | Base = 0
+    | Additive = 1
+
+type World =
+    {
+        Items : Item list
+        Characters : Character list
+    }
+
+and Character =
     {
         Details: Details
-        Str : int
-        Dex : int
-        Con : int
-        Int : int
-        Wis : int
-        Cha : int
-        Items : Item list
-        RightHand : Item
-        Armor   : Item
-        Level : float
-        Hitpoints : int
+        Strength : int
+        Dexterity : int
+        Inventory : Item list
+        RightHandEquipped : Item
     }
 
-//#endregion
-
-let EquipInRightHand item creature =
-    if List.contains item creature.Items then {creature with RightHand = item} else creature 
-
-let EquipInArmor item creature =
-    if List.contains item creature.Items then {creature with Armor = item} else creature
-
-
-let Give item creature =
-    {creature with Items = List.append creature.Items [item]}
-
-let ClampTo y x  =
-    if x < y then x else y
-
-let CalcToHit creature =
-    let result : int = match creature.RightHand with
-                       | Weapon(properties = p) ->
-                        if p.Finesse then max(StatToMod creature.Str, StatToMod creature.Dex) + ChalToProf creature.Level else
-                        if p.Ranged then StatToMod creature.Dex + ChalToProf creature.Level else
-                        StatToMod creature.Str + ChalToProf creature.Level
-
-    log (sprintf "%s gets an extra %i to their chance to hit" creature.Details.Name result) |> ignore
-    result
-              
-let CalcAC creature =
-    let result =
-        match creature.Armor with
-        | Armor(_, modifier, armorClass) ->
-            modifier + match armorClass with
-                       | ArmorClass.Light -> StatToMod creature.Dex
-                       | ArmorClass.Medium -> StatToMod creature.Dex |> ClampTo 2
-                       | ArmorClass.Heavy -> 0
-
-    log (sprintf "%s has an AC of %i" creature.Details.Name result) |> ignore      
-    result
-
-let Hits creature target =
-    CalcToHit creature + roll 20 > CalcAC target
-
-let CalcDamage creature =
-    let result =
-        match creature.RightHand with
-        | Weapon (dice = d) ->
-            roll d
-    log (sprintf "%s dealt %i" creature.Details.Name result) |> ignore
-    result
-
-let Attack creature target =
-    log (sprintf "%s attacking %s" creature.Details.Name target.Details.Name) |> ignore
-    if Hits creature target then CalcDamage creature else 0
-
-let RemoveHitpoints creature x =
-    let result = {creature with Hitpoints = creature.Hitpoints - x}
-    log (sprintf "%s now has %i hitpoints" creature.Details.Name result.Hitpoints) |> ignore
-    result
-
-// #region records
-
-let leatherArmor : Item =
-    Armor ({ Name = "Leather Armor"; Description = "Stylish Leather Armor"; },
-           13,
-           ArmorClass.Light )
-
-let chainMail : Item =
-    Armor ({ Name = "Chain Mail"; Description = "Scary Chain Mail"; },
-           16,
-           ArmorClass.Heavy )
-
-let noArmor : Item =
-    Armor ({ Name = "No Armor"; Description = "Butt naked"; },
-           0,
-           ArmorClass.Light )
-
-let scimitar : Item =
-    Weapon ({ Name = "Scimitar"; Description = "A Scimitar"; },
-            6,
-            { Finesse = true; Ranged = false; })
-
-let longsword : Item =
-    Weapon ({ Name = "Longsword"; Description = "A Longsword"; },
-            10,
-            { Finesse = false; Ranged = false; })
-
-
-let emptyHand : Item =
-    Weapon ({ Name = "Empty Hand"; Description = "Bare fists"; },
-            1,
-            { Finesse = false; Ranged = false; })
-
-let mutable goblin =
+and WeaponProperty =
     {
-        Details = { Name = "Goblin"; Description = "A mean looking goblin"};
-        Str=8; Dex=14; Con=10; Int=10; Wis=10; Cha=9;
-        Items = [];
-        Armor = noArmor;
-        Hitpoints = 7;
-        RightHand = emptyHand;
-        Level = 0.25;
+        ExecutionPoint : ItemPropertyExecutionPoint
+        CompositionStyle : ItemPropertyCompositionStyle
+        Function : Character -> int
     }
+    member this.Execute =
+        this.Function
 
-let mutable hobGoblin =
+and ItemKind = 
+    | Weapon of diceDamage: int * properties: WeaponProperty list
+    | Armor of modifier: int * armorClass: ArmorClass
+
+and Item =
     {
-        Details = { Name = "Hobgoblin"; Description = "A really mean looking goblin"};
-        Str=13; Dex=12; Con=12; Int=10; Wis=10; Cha=9;
-        Items = [];
-        Armor = noArmor;
-        Hitpoints = 11;
-        RightHand = emptyHand;
-        Level = 0.5;
+        Details : Details
+        Kind : ItemKind
     }
 
-// #endregion
+let max x y =
+    if x > y then x else y 
+
+let add x y =
+    x + y
+
+let applyBaseModifiers creature modifiers =
+    modifiers
+    |> List.filter (fun n -> n.CompositionStyle = ItemPropertyCompositionStyle.Base)
+    |> List.map (fun n -> n.Execute creature) 
+    |> List.reduce max
+
+let applyAdditiveModifiers creature modifiers =
+    modifiers
+    |> List.filter (fun n -> n.CompositionStyle = ItemPropertyCompositionStyle.Additive)
+    |> List.map (fun n -> n.Execute creature) 
+    |> List.reduce add
+
+let applyModifiers creature modifiers =
+    applyBaseModifiers creature modifiers + applyAdditiveModifiers creature modifiers
+
+let getToHitModifier creature weapon =
+    match creature.RightHandEquipped.Kind with
+    | Weapon(_, properties) ->
+        properties 
+        |> applyModifiers creature
+    | _ ->
+        0
+
+let finesse =
+    {
+        ExecutionPoint = ItemPropertyExecutionPoint.ToHitModCalc;
+        CompositionStyle = ItemPropertyCompositionStyle.Base;
+        Function = fun x -> max x.Dexterity x.Strength;
+    }
+
+let scimitar =
+    {
+        Details = { Identifier = "weapon_scimitar"; Name = "Scimitar"; Description = "A curved sword" };
+        Kind = Weapon(6, [finesse]);
+    }
+
+let goblin =
+    {
+        Details = { Identifier = "creature_goblin_1"; Name = "Goblin"; Description = "A Scary Goblin" };
+        Strength = 8;
+        Dexterity = 14;
+        Inventory = [scimitar];
+        RightHandEquipped = scimitar;
+    }
+
+let world =
+    {
+        Characters = [goblin];
+        Items = [scimitar];
+    }
 
 [<EntryPoint>]
 let main argv =
-
-    goblin <- Give scimitar goblin 
-    |> Give leatherArmor 
-    |> EquipInRightHand scimitar 
-    |> EquipInArmor leatherArmor
-
-    hobGoblin <- Give longsword hobGoblin 
-    |> Give chainMail 
-    |> EquipInRightHand longsword 
-    |> EquipInArmor chainMail
-
-    hobGoblin <- Attack goblin hobGoblin
-    |> RemoveHitpoints hobGoblin
-
-    goblin <- Attack hobGoblin goblin
-    |> RemoveHitpoints goblin
-
-    let mutable Break = false
-
-    while not Break do
-        if goblin.Hitpoints > 0 then
-            hobGoblin <- Attack goblin hobGoblin
-            |> RemoveHitpoints hobGoblin
-        else
-            for item in combatLog do
-                printfn "%s" item
-            printf "the goblin is dead!"
-            Break <-true
-
-        if hobGoblin.Hitpoints > 0 then
-            goblin <- Attack hobGoblin goblin
-            |> RemoveHitpoints goblin
-        else 
-            for item in combatLog do
-                printfn "%s" item
-            printf "the hobgoblin is dead!"
-            Break <-true
-
     
-
     0
-    
