d3c3afb42b88166cca053b71f893dbacd57b4a60
Rasengangstarr <davidouthere@msn.com>
2021-01-25 19:47:21 +0000

    getting a bit silly now

--- Program.fs
+++ Program.fs
@@ -1,5 +1,11 @@
 open System
 
+let stat2Mod x =
+    (x - 10) / 2
+    |> float
+    |> floor
+    |> int
+
 type Details =
     {
         Identifier : string
@@ -25,6 +31,7 @@ type World =
     {
         Items : Item list
         Characters : Character list
+        
     }
 
 and Character =
@@ -34,7 +41,11 @@ and Character =
         Dexterity : int
         Inventory : Item list
         RightHandEquipped : Item
+        LeftHandEquipped : Item
+        ArmorEquipped : Item
     }
+    member this.StrMod = stat2Mod this.Strength
+    member this.DexMod = stat2Mod this.Dexterity
 
 and WeaponProperty =
     {
@@ -47,7 +58,7 @@ and WeaponProperty =
 
 and ItemKind = 
     | Weapon of diceDamage: int * properties: WeaponProperty list
-    | Armor of modifier: int * armorClass: ArmorClass
+    | Armor of armorClass: ArmorClass
 
 and Item =
     {
@@ -61,40 +72,106 @@ let max x y =
 let add x y =
     x + y
 
-let applyBaseModifiers creature modifiers =
+let log m f =
+    let r = f
+    printf m r
+    r
+
+let roll s =
+    let r = System.Random()
+    r.Next(1,s)
+
+let getModifiersForExecutionPoint t l =
+    List.filter (fun n -> n.ExecutionPoint = t) l
+
+let getModifiersForStyle t l =
+    List.filter (fun n -> n.CompositionStyle = t) l
+
+let aggregateModifiers creature modifiers s f =
     modifiers
-    |> List.filter (fun n -> n.CompositionStyle = ItemPropertyCompositionStyle.Base)
+    |> getModifiersForStyle s
     |> List.map (fun n -> n.Execute creature) 
-    |> List.reduce max
+    |> fun a -> match a.Length with
+                | 0 -> 0
+                | _ -> a 
+                       |> List.reduce f
 
 let applyAdditiveModifiers creature modifiers =
-    modifiers
-    |> List.filter (fun n -> n.CompositionStyle = ItemPropertyCompositionStyle.Additive)
-    |> List.map (fun n -> n.Execute creature) 
-    |> List.reduce add
+    aggregateModifiers creature modifiers ItemPropertyCompositionStyle.Additive add
+    
+let applyBaseModifiers creature modifiers =
+    aggregateModifiers creature modifiers ItemPropertyCompositionStyle.Base max
 
 let applyModifiers creature modifiers =
     applyBaseModifiers creature modifiers + applyAdditiveModifiers creature modifiers
 
-let getToHitModifier creature weapon =
+let getModifier executionPoint creature = 
     match creature.RightHandEquipped.Kind with
-    | Weapon(_, properties) ->
-        properties 
+    | Weapon(_, properties) -> 
+        properties
+        |> getModifiersForExecutionPoint executionPoint
         |> applyModifiers creature
     | _ ->
         0
 
-let finesse =
+let getDamageModifier creature =
+    getModifier ItemPropertyExecutionPoint.DamageCalc creature
+
+let getToHitModifier creature =
+    getModifier ItemPropertyExecutionPoint.ToHitModCalc creature
+
+let getToHit creature =
+    log "rolled to hit: %i" (roll 20)
+    + log " + %i \n" (getToHitModifier creature)
+
+let getBaseDamage creature =
+    match creature.RightHandEquipped.Kind with
+    | Weapon(dice, _) -> 
+        dice
+        |> roll
+    | _ ->
+        0
+
+let getDamage creature =
+    log "rolled damage: %i" (getBaseDamage creature)
+    + log " + %i \n" (getDamageModifier creature)
+
+let attack creature =
+    match getToHit creature with
+    | 0 -> 0
+    | _ -> getDamage creature
+
+let finesseHit =
     {
         ExecutionPoint = ItemPropertyExecutionPoint.ToHitModCalc;
         CompositionStyle = ItemPropertyCompositionStyle.Base;
-        Function = fun x -> max x.Dexterity x.Strength;
+        Function = fun x -> max x.DexMod x.StrMod;
+    }
+
+let finesseDamage =
+    {
+        ExecutionPoint = ItemPropertyExecutionPoint.DamageCalc;
+        CompositionStyle = ItemPropertyCompositionStyle.Base;
+        Function = fun x -> max x.DexMod x.StrMod;
+    }
+
+let melee =
+    {
+        ExecutionPoint = ItemPropertyExecutionPoint.ToHitModCalc;
+        CompositionStyle = ItemPropertyCompositionStyle.Base;
+        Function = fun x -> x.StrMod;
     }
 
 let scimitar =
     {
         Details = { Identifier = "weapon_scimitar"; Name = "Scimitar"; Description = "A curved sword" };
-        Kind = Weapon(6, [finesse]);
+        Kind = Weapon(6, [finesseHit; finesseDamage; melee]);
+    }
+
+let leatherArmor =
+    {
+        Details = { Identifier = "armor_leather"; Name = "Leather Armor"; Description = "some leather armor" };
+        Kind = Weapon(6, [finesseHit; melee]);
     }
 
 let goblin =
@@ -104,15 +181,14 @@ let goblin =
         Dexterity = 14;
         Inventory = [scimitar];
         RightHandEquipped = scimitar;
+        LeftHandEquipped = scimitar;
+        ArmorEquipped = scimitar;
     }
 
-let world =
-    {
-        Characters = [goblin];
-        Items = [scimitar];
-    }
 
 [<EntryPoint>]
 let main argv =
+    goblin
+    |> attack
     
     0
